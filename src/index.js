b3a1b10454465271b7eb91f04026f97d3bc6ac53
maJestic17 <77455932+maJestic17@users.noreply.github.com>
2021-02-16 01:34:51 +0300

    Implemented Forecast

--- src/index.js
+++ src/index.js
@@ -15,10 +15,33 @@ let iconElement = document.querySelector("#icon");
 iconElement.setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}.png`);
 iconElement.setAttribute("alt", response.data.weather[0].description);
 }
+function displayForecast(response) {
+let forecastElement = document.querySelector("#forecast");
+forecastElement.innerHTML = null;
+let forecast = null;
+
+for (let index = 0; index < 5; index++) {
+forecast = response.data.list[index];
+forecastElement.innerHTML+= `
+<div class="col-2">${formatHours(forecast.dt * 1000)} 
+    <br/>
+   ${Math.round(forecast.main.temp_max)}° /${Math.round(forecast.main.temp_min)}° <br/>
+    
+        <img
+        src="http://openweathermap.org/img/wn/${forecast.weather[0].icon}@2x.png"
+        />
+    </div>
+    </div>
+`;   
+}
+}
+
 function search(city){
 let apiKey = "78077238f4ea41548bb4d14fbbe9813d";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 axios.get(apiUrl).then(showTemperature);
+apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=metric`;
+axios.get(apiUrl).then(displayForecast);
 }
 function displayFahrenheitTemperature(event) {
 event.preventDefault();
@@ -50,6 +73,19 @@ event.preventDefault();
 let city=document.querySelector("#search-text-input").value;
 search(city);  
 }
+
+function formatHours(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  return `${hours}:${minutes}`;
+}
 let now = new Date();
 let date = document.querySelector("#date");
 let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
@@ -65,7 +101,6 @@ if (minutes<10){
 date.innerHTML=`${day} ${hours}:${minutes}`;
 let form = document.querySelector("#search-form");
 form.addEventListener("submit", submitSearch);
-
 function showCurrent(event){
 event.preventDefault();
 function currentTemperature(response) {
